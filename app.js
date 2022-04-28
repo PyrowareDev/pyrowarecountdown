f329d881ff5c6f50f22a9ffc39a54140965ff38a
Dam-Dev <damiano.tavani@gmail.com>
2022-04-29 00:11:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const countdown = () => {
+    const countDate = new Date("May 14, 2022").getTime();
+    const now = new Date().getTime()
+    const gap = countDate - now
+    const secondi =  1000;
+    const minuti = secondi * 60;
+    const ore = minuti * 60;
+    const giorni = ore * 24;
+
+    const giornoApertura = Math.floor(gap / giorni);
+    const oreApertura = Math.floor((gap % giorni) / ore);
+    const minutiApertura = Math.floor((gap % ore) / minuti);
+    const secondiApertura = Math.floor((gap % minuti) / secondi);
+    
+    setInnerTextByID("giorni", giornoApertura)
+    setInnerTextByID("ore", oreApertura)
+    setInnerTextByID("minuti", minutiApertura)
+    setInnerTextByID("secondi", secondiApertura)
+    
+}
+
+function setInnerTextByID(id, text){
+    document.querySelector(`#${id}`).innerText = text;
+}
+setInterval(countdown, 1000)
