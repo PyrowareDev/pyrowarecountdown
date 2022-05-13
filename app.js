f2ba98a013131374b76782e193fd53a2f3ca4d12
Dam-dev <damiano.tavani@gmail.com>
2022-05-13 13:52:51 +0200

    Modificata la data

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const countdown = () => {
-    const countDate = new Date("May 14, 2022").getTime();
-    const now = new Date().getTime()
-    const gap = countDate - now
-    const secondi =  1000;
-    const minuti = secondi * 60;
-    const ore = minuti * 60;
-    const giorni = ore * 24;
+  const countDate = new Date("May 21, 2022").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+  const secondi = 1000;
+  const minuti = secondi * 60;
+  const ore = minuti * 60;
+  const giorni = ore * 24;
 
-    const giornoApertura = Math.floor(gap / giorni);
-    const oreApertura = Math.floor((gap % giorni) / ore);
-    const minutiApertura = Math.floor((gap % ore) / minuti);
-    const secondiApertura = Math.floor((gap % minuti) / secondi);
-    
-    setInnerTextByID("giorni", giornoApertura)
-    setInnerTextByID("ore", oreApertura)
-    setInnerTextByID("minuti", minutiApertura)
-    setInnerTextByID("secondi", secondiApertura)
-    
-}
+  const giornoApertura = Math.floor(gap / giorni);
+  const oreApertura = Math.floor((gap % giorni) / ore);
+  const minutiApertura = Math.floor((gap % ore) / minuti);
+  const secondiApertura = Math.floor((gap % minuti) / secondi);
 
-function setInnerTextByID(id, text){
-    document.querySelector(`#${id}`).innerText = text;
+  setInnerTextByID("giorni", giornoApertura);
+  setInnerTextByID("ore", oreApertura);
+  setInnerTextByID("minuti", minutiApertura);
+  setInnerTextByID("secondi", secondiApertura);
+};
+
+function setInnerTextByID(id, text) {
+  document.querySelector(`#${id}`).innerText = text;
 }
-setInterval(countdown, 1000)
+setInterval(countdown, 1000);
